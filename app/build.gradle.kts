58ff4d6b684ee2fc8ef27b77f478f28e027a0b13
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-10-25 03:13:25 +0700

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("kotlin-android")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.dicoding.newsapp"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.dicoding.newsapp"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+        buildConfigField("String", "API_KEY", "\"bd5abd173d1948849300fa69a592d5c5\"")
+    }
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    // ui
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+
+    implementation(libs.glide)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.fragment.ktx)
+
+    //testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.room.compiler)
+
+    //retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+    //coroutine support
+        // Lifecycle Components
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)  // ViewModelScope
+    implementation(libs.androidx.lifecycle.livedata.ktx)   // LiveData
+
+        // Room Components
+    implementation(libs.room.ktx)                          // Room KTX
+}
